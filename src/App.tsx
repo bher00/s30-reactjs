c48fb48cb6158ad7cbd4249ca73e0a0fcdbf81d8
bher00 <bhdrms0614@gmail.com>
2023-07-13 16:32:00 +0800

    update and use reusable components to display fullname, email address and refresh button

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import type { ReactElement } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useEffect, useState } from 'react';
+import { Label, RefreshButton } from '~/components';
+import { getRandomUsers } from '~/apis/user';
+import { getEmailAddress, getFullName } from '~/helper';
+
+const App = (): ReactElement => {
+  const [randomUserLoading, setRandomUserLoading] = useState(false);
+  const [fullname, setFullname] = useState('-');
+  const [email, setEmail] = useState('-');
+
+  const handleGetRandomUsers = async (): Promise<void> => {
+    setRandomUserLoading(true);
+    const user = await getRandomUsers();
+    const results = user?.results?.[0];
+
+    const userFullName = getFullName(results?.name);
+    const userEmail = getEmailAddress(results);
+
+    setFullname(userFullName);
+    setEmail(userEmail);
+    setRandomUserLoading(false);
+  };
+
+  const handleRefreshButton = (): void => {
+    !randomUserLoading && handleGetRandomUsers();
+  };
+
+  useEffect(() => {
+    handleGetRandomUsers();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container">
+      <div className="refresh-button-container">
+        <RefreshButton
+          showIcon
+          loading={randomUserLoading}
+          onClick={handleRefreshButton}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Label value={fullname} title="Full name" />
+      <Label value={email} title="Email Address" />
+    </div>
+  );
+};
 
-export default App
+export default App;
