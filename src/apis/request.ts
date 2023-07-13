ab97d4d290a8b44105b7cd884a16d8cfc9ac9f87
bher00 <bhdrms0614@gmail.com>
2023-07-13 16:23:59 +0800

    add randomUser request

--- src/apis/request.ts
+++ src/apis/request.ts
@@ -5,3 +5,7 @@ import axios from 'axios';
 export const createRequest = (config?: AxiosRequestConfig): AxiosInstance => {
   return axios.create(config);
 };
+
+export const randomUserRequest = createRequest({
+  baseURL: 'https://randomuser.me',
+});
