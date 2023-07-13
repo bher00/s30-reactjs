15c91f3b304aef13327b5edf72f0595f6d9f9bd0
bher00 <bhdrms0614@gmail.com>
2023-07-14 04:11:37 +0800

    add vitest configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
+
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/__tests/config.ts', './src/setup-test.ts'],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: vitestConfig.test,
 });
